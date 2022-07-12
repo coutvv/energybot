e2c295a60e628d7058b2b7e68b9c9b1e5cbf9a40
Roman Lomovtsev <coutvv@gmail.com>
2022-07-12 11:32:26 +0300

    rm study files

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -5,12 +5,9 @@ import (
 	"log"
 	"os"
 
-	"github.com/coutvv/energybot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
-
-var productService = product.NewService()
 
 func main() {
 	godotenv.Load()
@@ -37,8 +34,6 @@ func main() {
 			switch message.Command() {
 			case "help":
 				helpCommand(bot, message)
-			case "list":
-				listCommand(bot, message)
 			case "registry":
 				registry(bot, message)
 			case "map":
@@ -56,15 +51,6 @@ func main() {
 			}
 		}
 	}
-}
-
-func listCommand(bot *tgbotapi.BotAPI, inputMsg *tgbotapi.Message) {
-	products := "Here all products\n\n"
-	for _, product := range productService.List() {
-		products += product.Title + "\n"
-	}
-	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, products)
-	bot.Send(msg)
 }
 
 func defaultBehavior(bot *tgbotapi.BotAPI, inputMsg *tgbotapi.Message) {
