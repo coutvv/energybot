b6485e73673ae4599fe9db40ee3686f571c29d7a
Roman Lomovtsev <coutvv@gmail.com>
2022-08-13 00:49:55 +0300

    change phase cmd

--- internal/energy/tg/always.go
+++ internal/energy/tg/always.go
@@ -1,7 +1,7 @@
 package tg
 
 import (
-	"fmt"
+	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -9,6 +9,21 @@ import (
 func (ch *CommandHelper) phase(inputMsg *tgbotapi.Message) {
 	chatId := inputMsg.Chat.ID
 	game, _ := ch.Manager.Repository.GetUnfinishedGame(chatId)
+	var phase string
+	switch game.GamePhase {
+	case 0:
+		phase = "torgi"
+	case 1:
+		phase = "torgi"
+	case 2:
+		phase = "resources"
+	case 3:
+		phase = "domiki"
+	case 4:
+		phase = "money"
+	default:
+		log.Fatal("incorrect phase of this game")
+	}
 
-	ch.sendMessage(chatId, "Current phase is: "+fmt.Sprint(game.GamePhase))
+	ch.sendMessage(chatId, "Current phase is: "+phase)
 }
